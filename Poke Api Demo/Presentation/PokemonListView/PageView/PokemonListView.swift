6402d62cfe9ef5fceedff439174729014e8dc4d7
jesuspedge <roger.bacab7@gmail.com>
2026-03-05 10:14:04 -0600

    refactor: changing presentation folder structure

--- Poke Api Demo/Presentation/PokemonListView/PageView/PokemonListView.swift
+++ Poke Api Demo/Presentation/PokemonListView/PageView/PokemonListView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SDWebImageSwiftUI
 
 struct PokemonListView: View {
     @State private var viewModel = PokemonListViewModel()
@@ -55,46 +54,6 @@ struct PokemonListView: View {
         .onAppear {
             Task { await viewModel.fetchPokemons() }
         }
-    }
-}
-
-
-
-struct PokemonCard: View {
-    let pokemon: PokemonEntity
-
-    @Environment(\.colorScheme) var colorScheme
-
-    var body: some View {
-        HStack {
-            VStack {
-                Text(pokemon.name.capitalized)
-                    .font(.title)
-                    .foregroundStyle(pokemonTypeColorDic[pokemon.types.first!.name] ?? .black)
-                    .frame(maxWidth: .infinity, maxHeight: 25, alignment: .leading)
-
-
-                HStack {
-                    Image(systemName: pokemonTypeIconDic[pokemon.types.first!.name] ?? "star.fill")
-                        .foregroundStyle(pokemonTypeColorDic[pokemon.types.first!.name] ?? .black)
-                    Text(pokemon.types.first!.name.capitalized)
-                }
-                .frame(maxWidth: .infinity, maxHeight: 25, alignment: .leading)
-            }
-            .padding(.leading, 15)
-
-            WebImage(url: URL(string: pokemon.sprites.officialArtworkURL))
-                .resizable()
-                .frame(width: 170, height: 170)
-                .padding(.top, 30)
-                .padding(.leading, -10)
-
-        }
-        .frame(maxWidth: .infinity,  maxHeight: 150)
-        .background(colorScheme == .light ? Color.white : Color.gray.opacity(0.3))
-        .cornerRadius(10)
-        .shadow(radius: 5)
-
     }
 }
 
